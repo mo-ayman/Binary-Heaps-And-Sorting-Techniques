0c6624f668f0957f4b6df4c11ad41343b2592964
LouayMagdy <magdylouay@gmail.com>
2022-04-02 14:14:39 +0200

    main class

--- src/HeapAndSort/Main.java
+++ src/HeapAndSort/Main.java
@@ -1,28 +1,133 @@
 package HeapAndSort;
 
-import HeapAndSort.Sorting.HeapSort;
-import HeapAndSort.Sorting.QuickSort;
-import HeapAndSort.Sorting.Sorter;
+import HeapAndSort.BinHeap.BinaryHeap;
+import HeapAndSort.BinHeap.BinaryHeapMax;
+import HeapAndSort.Sorting.*;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
+import java.util.Scanner;
 
-public class Main {
+public class Main{
+    static Scanner reader = new Scanner(System.in);
+
     public static void main(String[] args) {
-        List<Integer> temp = new ArrayList<>();
-        for (int i = 0; i < 10; i++){
-            Random value = new Random();
-            Integer item = (value.nextInt(100));
-            System.out.println(item);
-            temp.add(item);
+        System.out.println("Select An Option: \n 1. To Use The Heap\n 2. To sort some list of data\n Any Other Key. exit");
+        System.out.print("=> ");
+        byte option;
+        boolean exit = false;
+        while (!exit){
+            try {
+                option = reader.nextByte();
+                switch (option) {
+                    case 1 -> testHeap();
+                    case 2 -> sort();
+                    default -> exit = true;
+                }
+            }
+            catch (Exception e){
+                exit = true;
+            }
         }
-        Sorter<Integer> sorter = new HeapSort<>();
-        Sorter<Integer> sorter1 = new QuickSort<>();
-        temp = sorter1.sort(temp);
-//        temp = sorter.sort(temp);
-        for (int i = 0; i < 10; i++){
-            System.out.println(":"+temp.get(i));
+    }
+    static void testHeap(){
+        BinaryHeap<Integer> binaryHeap = new BinaryHeapMax<>(1000);
+        byte option;
+        boolean exit = false;
+        while (! exit){
+            System.out.println("Select A Process: \n 1. Insert\n 2. Delete Max item \n 3. Get Max item\n 4. Build From Scratch\n Any Other Key. exit");
+            System.out.print("=> ");
+            option = reader.nextByte();
+            try {
+                switch (option){
+                    case 1:
+                        System.out.print("Enter An item to insert: ");
+                        binaryHeap.insert(reader.nextInt());
+                        break;
+                    case 2:
+                        if(binaryHeap.deleteMax()) System.out.println("Deleted Successfully");
+                        else System.out.println("The Heap is empty !!");
+                        break;
+                    case 3:
+                        System.out.println("The Max. item : " + binaryHeap.getMax());
+                        break;
+                    case 4:
+                        binaryHeap.build(getIntList());
+                        System.out.println("Heap built");
+                        break;
+                    default:
+                        exit = true;
+                }
+            }
+            catch (Exception e){
+                exit = true;
+            }
         }
+    }
+    static void sort(){
+        Sorter<Integer> sorter;
+        boolean exit = false;
+        byte option = reader.nextByte();
+        List<Integer> result = new ArrayList<>();
+        while (!exit){
+            System.out.println("Select A Sorting Technique: \n");
+            System.out.println(" 1. Selection Sort  2. Bubble Sort  3. Insertion Sort\n 4. Merge Sort  5. Quick Sort  6. Heap Sort\n Any Other Key. exit");
+            System.out.print("=> ");
+            try {
+                switch (option) {
+                    case 1 -> {
+                        sorter = new SelectionSort<>();
+                        result = sorter.sort(getIntList());
+                    }
+                    case 2 -> {
+                        sorter = new BubbleSort<>();
+                        result = sorter.sort(getIntList());
+                    }
+                    case 3 -> {
+                        sorter = new InsertionSort<>();
+                        result = sorter.sort(getIntList());
+                    }
+                    case 4 -> {
+                        sorter = new MergeSort<>();
+                        result = sorter.sort(getIntList());
+                    }
+                    case 5 -> {
+                        sorter = new QuickSort<>();
+                        result = sorter.sort(getIntList());
+                    }
+                    case 6 -> {
+                        sorter = new HeapSort<>();
+                        result = sorter.sort(getIntList());
+                    }
+                    default -> exit = true;
+                }
+            }
+            catch (Exception e){
+                exit = true;
+            }
+            if(!exit){
+                for(Integer num: result) System.out.print(num + " ");
+                System.out.println();
+            }
+        }
+    }
+
+    private static List<Integer> getIntList(){
+        Scanner listReader = new Scanner(System.in);
+        List<Integer> list = new ArrayList<>();
+        System.out.print("Enter The List Size: ");
+        int size = listReader.nextInt();
+        System.out.println("Enter A List(element per line): ");
+        for(int i = 0; i < size; i++) list.add(listReader.nextInt());
+        return list;
+    }
+    private static List<Double> getDoubleList(){
+        Scanner listReader = new Scanner(System.in);
+        List<Double> list = new ArrayList<>();
+        System.out.print("Enter The List Size: ");
+        int size = listReader.nextInt();
+        System.out.println("Enter A List(element per line): ");
+        for(int i = 0; i < size; i++) list.add(listReader.nextDouble());
+        return list;
     }
 }
